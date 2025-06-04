404c75bf3e8a229ccbca3c46fa94cd5b090c523a
Your Name <you@example.com>
2025-06-04 14:13:51 +0400

    fixed errors

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import yfinance as yf
 import subprocess
+
 subprocess.run("git add .",shell=True,check=True)
+
 result = subprocess.getoutput("git status --porcelain")
 if result.strip():
     print("something to commit")
